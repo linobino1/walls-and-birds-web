94c0a28708c2fd4f6f14719765c3c0a3d2f73fe3
Leo <leo.hilsheimer@gmail.com>
2023-11-17 09:32:59 +0100

    add song transpose logic

--- app/routes/songbook.$song/route.tsx
+++ app/routes/songbook.$song/route.tsx
@@ -1,7 +1,12 @@
 import { json, type LoaderFunctionArgs } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import {
+  isRouteErrorResponse,
+  useLoaderData,
+  useRouteError,
+} from "@remix-run/react";
 import classes from "./index.module.css";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { transposeChord, transposeMap } from "./util";
 
 export const loader = async ({
   context: { payload },
@@ -17,15 +22,40 @@ export const loader = async ({
     limit: 1,
   });
 
-  if (!song.docs.length) throw new Error("Song not found");
+  if (!song.docs.length) throw new Response("Song not found", { status: 404 });
 
   return json({ song: song.docs[0] }, { status: 200 });
 };
 
+export function ErrorBoundary() {
+  const error = useRouteError();
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div>
+        <p>Sorry, we couldn't find that song.</p>
+      </div>
+    );
+  }
+}
+
 export default function Song() {
   const { song } = useLoaderData<typeof loader>();
-  const transpose = (amount: number) => {};
   const content = useRef<HTMLDivElement>(null);
+
+  // Transpose logic
+  const [key, setKey] = useState(song?.key || "C ");
+  const transpose = (steps: number) => {
+    // HACK: "A" is not valid chord, but "A " is
+    // reason: we need to parse the song content to find chords, and we don't want to replace all "A" letters in the song
+    setKey(transposeChord(`${key.trim()} `, steps));
+
+    if (content.current?.innerHTML) {
+      content.current.innerHTML = content.current.innerHTML.replace(
+        new RegExp(Object.keys(transposeMap).join("|"), "g"),
+        (matched) => transposeMap[matched] as string
+      );
+    }
+  };
 
   return song ? (
     <div className={classes.container}>
@@ -33,9 +63,7 @@ export default function Song() {
         {song.by} - {song.title}
       </h2>
       <p>
-        <span>tom: </span>
-        <button onClick={() => transpose(-1)}>-</button>
-        <span data-transpose>&nbsp;{song.key}&nbsp;</span>
+        tom: <button onClick={() => transpose(-1)}>-</button> {key.trim()}{" "}
         <button onClick={() => transpose(1)}>+</button>
       </p>
 
