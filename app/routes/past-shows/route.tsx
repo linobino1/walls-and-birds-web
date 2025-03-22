9f201c1256f3ecff08f060fc8ab6b553c29b48b9
Leo <leo.hilsheimer@gmail.com>
2025-03-22 19:01:12 +0100

    chore: exclude upcoming shows from past shows

--- app/routes/past-shows/route.tsx
+++ app/routes/past-shows/route.tsx
@@ -5,10 +5,18 @@ import { useLoaderData } from "@remix-run/react";
 import { Shows } from "~/components/Shows";
 
 export const loader = async ({ context: { payload } }: LoaderFunctionArgs) => {
+  // today, 00:00:00
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
   const shows = await payload.find({
     collection: "shows",
     sort: "-date",
     pagination: false,
+    where: {
+      date: {
+        less_than: today,
+      },
+    },
   });
 
   return json({ shows }, { status: 200 });
