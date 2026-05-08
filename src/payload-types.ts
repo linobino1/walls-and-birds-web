d64ccddb60e8fefcf0268dbb5a11218edf7fedbf
Leo <leo.hilsheimer@gmail.com>
2026-05-08 07:45:11 +0200

    chore: upgrade deps payload, react, nextjs

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -100,9 +100,10 @@ export interface Config {
     faq: FaqSelect<false> | FaqSelect<true>;
   };
   locale: null;
-  user: User & {
-    collection: 'users';
+  widgets: {
+    collections: CollectionsWidget;
   };
+  user: User;
   jobs: {
     tasks: unknown;
     workflows: unknown;
@@ -259,6 +260,7 @@ export interface User {
       }[]
     | null;
   password?: string | null;
+  collection: 'users';
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
@@ -530,6 +532,16 @@ export interface FaqSelect<T extends boolean = true> {
   updatedAt?: T;
   createdAt?: T;
   globalType?: T;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "collections_widget".
+ */
+export interface CollectionsWidget {
+  data?: {
+    [k: string]: unknown;
+  };
+  width: 'full';
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
